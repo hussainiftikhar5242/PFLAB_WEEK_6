440218d8f400fa0be2dd763b02dc52c22c8e779c
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-26 01:16:51 +0500

    calculate the square root of the number

---
+++ task1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    float number;
+    cout<<"enter a number:";
+    cin>> number;
+    double squreroot=sqrt(number);
+    cout<<"suare root of the given number is "<< squreroot <<endl;
+    
+}
