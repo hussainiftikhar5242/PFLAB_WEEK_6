8864debf727efacd5b43aaba16b6d71ca841bfe5
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-26 01:17:41 +0500

    calculate the power 2 of the number.

---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main()
+{
+    int number;
+    cout<<"enter a number:";
+    cin >> number;
+    int power1 = pow (number , 2);
+    cout << "suare of the number is :" << power1 << endl; 
+
+}
