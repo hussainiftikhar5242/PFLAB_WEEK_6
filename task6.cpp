b4a5670a82e92d4807e69f078a0b4793a696ab4d
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-26 01:39:22 +0500

    make void function that display message.

---
+++ task6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void hello(); // function declration.
+main()//start of mai function
+{
+    hello(); // calling of function
+}//end of main
+void hello ()
+{
+    cout<<"hello world:"; 
+}
