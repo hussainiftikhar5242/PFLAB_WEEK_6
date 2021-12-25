d7fb3183c3ca39def7ee844d0fb795ee3dac2bb4
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-26 01:19:43 +0500

    calculate the sine of the number

---
+++ task3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main()
+{
+    double number;
+    cout<< "enter a number:";
+    cin >> number;
+    float number1=sin(number);
+    cout<< "sin of the given number is:" << number1 <<endl; 
+
+}
